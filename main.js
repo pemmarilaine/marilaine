59c1bc50685e415ca67654e71174064b21d272ae
pemmarilaine <127546927+pemmarilaine@users.noreply.github.com>
2024-04-02 08:24:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const bototes = document.querySelectorAll(".botao");
 for(let i=0; i<bototes.length; i++){
     botoes[i].onclick = function(){
-        for(let j=0; )
+        for(let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo);
     }
+        botoes[i].classList.add("ativo);
 }
