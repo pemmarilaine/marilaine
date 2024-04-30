cac4eef69250fe0cfb81762b5444d67295ea0b81
pemmarilaine <127546927+pemmarilaine@users.noreply.github.com>
2024-04-30 07:52:32 -0300

    main

--- main.js
+++ main.js
@@ -11,8 +11,10 @@ for(let i=0; i<botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-
+const tempoObjetivo1 = new Date("2024-12-14T00:00:00");
+const tempoObjetivo2 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo3 = new Date("2040-03-30T00:00:00");
+const tempoObjetivo4 = new Date("2030-10-20T00:00:00");
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
